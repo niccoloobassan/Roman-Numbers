e3680633522db81d10eba66a0c64ee74130a7d7c
Niccolò Bassan <bassan.niccolo@gmail.com>
2026-05-11 12:18:05 +0200

    feat: conversione primi 3 numeri (I, II, III)

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -4,16 +4,35 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 
+/**
+ * Classe per la conversione di numeri interi in numeri romani.
+ */
 public class IntegerToRoman {
+
+    private static final int[] VALUES = {1};
+    private static final String[] SYMBOLS = {"I"};
 
     /**
      * Converte un numero intero nel corrispondente numero romano.
      *
-     * @param number il numero da convertire
+     * @param number il numero da convertire (1-3)
      * @return la rappresentazione in numeri romani
+     * @throws IllegalArgumentException se il numero non e' valido
      */
     public static String convert(int number) {
-        // TODO: implementare la conversione
-        return "";
+        if (number < 1 || number > 3) {
+            throw new IllegalArgumentException(
+                "Il numero deve essere compreso tra 1 e 3, "
+                + "ricevuto: " + number);
+        }
+        StringBuilder roman = new StringBuilder();
+        int remaining = number;
+        for (int i = 0; i < VALUES.length; i++) {
+            while (remaining >= VALUES[i]) {
+                roman.append(SYMBOLS[i]);
+                remaining -= VALUES[i];
+            }
+        }
+        return roman.toString();
     }
 }
